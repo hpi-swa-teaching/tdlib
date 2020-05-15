c52d6bffc16cffd19f5cbf0650ccdfb36b5fb555
Schilli, Niklas <schilli@ams-berlin.de>
2020-05-16 01:52:01 +0200

    asd

--- build.ps1
+++ build.ps1
@@ -14,6 +14,6 @@ mkdir build
 mkdir release
 cd build
 cmake -A x64 -DCMAKE_INSTALL_PREFIX:PATH=../../release -DCMAKE_TOOLCHAIN_FILE:FILEPATH=../vcpkg/scripts/buildsystems/vcpkg.cmake ..
-cmake --build . --target install --config Release
+cmake --build . --target install --config Release 
 cd ..
 Compress-Archive -Path /release -DestinationPath /../release.zip
